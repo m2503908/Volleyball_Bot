05963dbfb50608e4d8f5cf84c2038f3c851a1cfa
Vitaliy Polyakov <vitaliypolyakov@MacBook-Air-Vitaliy.local>
2026-01-25 21:58:22 +0300

    Added file requirements.txt; Added file .env to keep bot token; Done base bot initialize with aiogram library

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
-print("123")
-print(123)
+import os
+from dotenv import load_dotenv
+
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
+
+
+load_dotenv()
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+@dp.message(Command(commands="start"))
+async def process_start_command(message: Message):
+    await message.answer('Здравствуйте! Убедитесь, что мы правильно распознали ваше имя: Имя1')
+
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
