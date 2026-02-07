7d70a259f3d9e6191cae8a6376b0d94aa1a7af91
Vitaliy Polyakov <vitaliypolyakov@MacBook-Air-Vitaliy.local>
2026-02-07 12:45:36 +0300

    Created function check_user

--- work_with_users_data.py
+++ work_with_users_data.py
@@ -4,3 +4,12 @@ def add_user(username, surname, name, telegram_id, subscribe=1, admin=0):
     with open('users_data.csv', newline='', mode='a') as f:
         writer = csv.writer(f, delimiter=';')
         writer.writerow([username, surname, name, telegram_id, subscribe, admin])
+
+
+def check_user(username):
+    with open('users_data.csv') as f:
+        reader = csv.DictReader(f, delimiter=';')
+        for row in reader:
+            if row['username'] == username:
+                return row['surname'], row['name']
+    return None, None
